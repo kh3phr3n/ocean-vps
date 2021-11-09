f5bb31a86fcd845ef5e2dd3598bb453363eeac0e
kh3phr3n <kh3phr3n@nixsys.fr>
2021-11-09 15:56:23 +0100

    Switch from dialog to whiptail

--- src/droplets/debian/setup.sh
+++ src/droplets/debian/setup.sh
@@ -21,7 +21,7 @@ USERHOME="/home/${USERNAME}"
 # $1: file
 show_log()
 {
-    dialog \
+    whiptail \
         --backtitle  "${BACKTITLE}" \
         --title      "Journal [*]" \
         --textbox    "$1" 24 62
@@ -32,7 +32,7 @@ check_env()
 {
     if [ -z "${ROOTPASS}" ] || [ -z "${USERPASS}" ] || [ -z "${USERNAME}" ]
     then
-        dialog \
+        whiptail \
             --backtitle "${BACKTITLE}" \
             --title     "Warning [!]" \
             --msgbox    "\nDon't forget to specify environment variables." 8 60
@@ -47,7 +47,7 @@ check_uid()
 {
     if [ "${UID}" -ne 0 ]
     then
-        dialog \
+        whiptail \
             --backtitle "${BACKTITLE}" \
             --title     "Warning [!]" \
             --msgbox    "\nRoot privileges are required for run this installer." 8 60
@@ -57,15 +57,65 @@ check_uid()
     fi
 }
 
+# Update package dependencies
+sys_upgrade()
+{
+    whiptail \
+        --backtitle "${BACKTITLE}" \
+        --title     "Confirmation [?]" \
+        --yesno     "\nDo you want to upgrade package dependencies?" 8 60
+
+    # 0 means user hit [yes] button
+    if [ "$?" -eq 0 ]
+    then
+        # Package maintenance
+        apt update && apt upgrade && apt autoremove
+    fi
+}
+
+# Install package dependencies
+sys_packages()
+{
+    whiptail \
+        --separate-output \
+        --backtitle "${BACKTITLE}" \
+        --title     "Selection [?]" \
+        --checklist "\nChoose which packages to install." 15 55 5 \
+            "1" "Utils: vim htop ranger ripgrep curl" ON \
+            "2" "Docker: docker.io docker-compose" ON \
+            "3" "Manpages: man-db manpages" ON \
+            "4" "Firewall: ufw netcat" ON \
+            2>${OUTPUT}
+
+    # 0 means user hit [yes] button
+    if [ "$?" -eq 0 ]
+    then
+        for item in $(<$OUTPUT)
+        do
+            case $item in
+                "1") apt install vim htop ranger ripgrep curl;;
+                "2") apt install docker.io docker-compose;;
+                "3") apt install man-db manpages;;
+                "4") apt install ufw netcat;;
+            esac
+        done
+    fi
+}
+
 # Program entry-point
 # -------------------
 
 # Controller and program flow
 main()
 {
+    clear
     # Run some checks
     check_env
     check_uid
+
+    # System actions
+    sys_upgrade
+    sys_packages
 
     # Set basic configuration
 
